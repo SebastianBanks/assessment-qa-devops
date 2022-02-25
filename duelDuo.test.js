ce225b53bcc94ce84e360d1ef11bd99a68eb4053
Sebastian Banks <Sebastian@thebanksfamily.net>
2022-02-25 10:00:16 -0700

    initial commit

---
+++ duelDuo.test.js
@@ -0,0 +1,19 @@
+
+const { Builder, Capabilities, By } = require("selenium-webdriver")
+require('chromedriver')
+const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
+
+
+beforeAll(async () => {
+    await driver.get('http://localhost:3000/')
+})
+
+afterAll(async () => {
+    await driver.quit()
+})
+
+test('Title shows up when page loads', async () => {
+    const title = await driver.findElement(By.id('title'))
+    const displayed = await title.isDisplayed()
+    expect(displayed).toBe(true)
+})
