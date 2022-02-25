1ec539300695c488624f6c13b95a30158d1a7777
Sebastian Banks <Sebastian@thebanksfamily.net>
2022-02-25 12:23:11 -0700

    finished

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -2,11 +2,12 @@
 const { Builder, Capabilities, By } = require("selenium-webdriver")
 require('chromedriver')
 const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
-const { } = require('./public/index.js')
+
 
 
 beforeAll(async () => {
     await driver.get('https://assessment-qa-devops-sb.herokuapp.com/')
+    await driver.sleep(1000)
 })
 
 afterAll(async () => {
@@ -15,31 +16,34 @@ afterAll(async () => {
 
 test('Title shows up when page loads', async () => {
     const title = await driver.findElement(By.id('title'))
+    await driver.sleep(1000)
     const displayed = await title.isDisplayed()
+    
     expect(displayed).toBe(true)
 })
 
 test('check draw button id === choices', async () => {
     const drawBtn = await driver.findElement(By.id("draw"))
+    await driver.sleep(1000)
     const choicesDiv = await driver.findElement(By.xpath("//section/div"))
+    await driver.sleep(1000)
     let equalToChoices = false
 
     await drawBtn.click()
-    console.log(choicesDiv.getAttributes("id"))
-    if (await choicesDiv.getAttributes("id") === "choices") {
+    await driver.sleep(1000)
+    
+    if (await choicesDiv.getAttribute("id") === "choices") {
         equalToChoices = true
     }
     expect(equalToChoices).toBe(true)
 })
 
 test('add to duo btn id === player-duo', async () => {
-    const drawBtn = await driver.findElement(By.id("draw"))
-    const addToDuoBtn = await driver.findElement(By.xpath("//section/div/div/button"))
-    const playerDiv = await driver.findElement(By.xpath('//section[2]/section/div'))
+    await driver.sleep(1000)
+    const playerDiv = await driver.findElement(By.xpath('//section[contains(@class,"container")]/section[1]/div'))
     let isPlayerDuo = false
-    drawBtn.click()
-    addToDuoBtn.click()
-    if (await playerDiv.getAttributes("id") === "player-duo") {
+    await driver.sleep(1000)
+    if (await playerDiv.getAttribute("id") === "player-duo") {
         isPlayerDuo = true
     }
     expect(isPlayerDuo).toBe(true)
